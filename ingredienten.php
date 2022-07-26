a319522431d180e345c69e4163051f4219a2506e
FelixvL <digitaleservice@gmail.com>
2022-07-26 14:21:10 +0200

    we gaan er iets bijmaken

--- ingredienten.php
+++ ingredienten.php
@@ -31,3 +31,37 @@ ontbijt:<input type=checkbox id=testcheckbox class=chkbxen onclick=gaan()>
 lunch:<input type=checkbox id=testcheckbox class=chkbxen onclick=gaan()>
 diner:<input type=checkbox id=testcheckbox class=chkbxen onclick=gaan()>
 <input type=hidden id="code">
+
+<h1>LASTIG LASTIG LASTIG</h1>
+<?php
+
+
+$sql = "SELECT ingredient.naam as naampje, recept.naam as benaming, ingredient.afbeelding as afb, recept.afbeelding as beeltenis
+FROM ingredient, recept, recept_ingredient_koppel
+WHERE ingredient.id = recept_ingredient_koppel.ingredient_id AND recept.id = recept_ingredient_koppel.recept_id
+ORDER BY recept.naam;";
+/*
+SELECT *
+FROM ingredient as I, recept as R, recept_ingredient_koppel as RI
+WHERE I.id = RI.ingredient_id AND R.id = RI.recept_id AND R.naam = 'broodrecept'
+ORDER BY R.naam;
+*/
+$conn = verbindDB();
+$rs = $conn -> query($sql);
+$vorignaam = "niks";
+
+while($rij = $rs->fetch_assoc()){
+    $afbeelding = $rij["afb"];
+    $naam = $rij["benaming"];
+    $ingredientnaam = $rij["naampje"];
+    if($vorignaam != $naam){
+        echo "<hr>";
+        echo "<h3>$naam</h3>";
+        $vorignaam = $naam;
+    }
+    echo "<h5>$ingredientnaam</h5>";
+    echo "<img src=img/$afbeelding width=200px>";
+    echo "<br>";
+}
+
+?>
